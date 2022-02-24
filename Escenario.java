83e3d94080b4c88587623f967e145a5174fdcc53
FrancoCC6 <98790152+FrancoCC6@users.noreply.github.com>
2022-02-24 16:11:09 -0300

    Update Escenario.java
    Lo hice una clase abstracta en lugar de una interfaz - Accion previa a replantear los escenarios como clases en lugar de JPanels

--- Escenario.java
+++ Escenario.java
@@ -1,3 +1,10 @@
-interface Escenario {
-  public void finalAction();
+public abstract class Escenario {
+  protected final Runnable ENDING_ACTION;
+  public final MouseListener MOUSE_LISTENER;
+  public final MouseMotionListener MOUSE_MOTION_LISTENER;
+  public final ImageIcon BG_IMAGE;
+  
+  public Escenario(Runnable r) {
+    ENDING_ACTION = r;
+  }
 }

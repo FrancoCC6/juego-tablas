00fea0cf4a0a689feb9fff5ee613913adac82e96
FrancoCC6 <98790152+FrancoCC6@users.noreply.github.com>
2022-02-25 00:39:12 -0300

    Update Escenario.java
    Agregado campo de panel asociado para repintar de manera interna; Modificados campos de listeners - ahora privados y con getters

--- Escenario.java
+++ Escenario.java
@@ -4,11 +4,17 @@ import javax.swing.*;
 
 public abstract class Escenario {
   protected final Runnable ENDING_ACTION;
-  public final MouseListener MOUSE_LISTENER;
-  public final MouseMotionListener MOUSE_MOTION_LISTENER;
-  public final ImageIcon BG_IMAGE;
+  protected MouseListener mouse_listener;
+  protected MouseMotionListener mouse_motion_listener;
+  protected ImageIcon bg_imageicon;
+  protected final JPanel PANEL_ASOCIADO;
   
-  public Escenario(Runnable r) {
+  public Escenario(Runnable r, JPanel p) {
     ENDING_ACTION = r;
+    PANEL_ASOCIADO = p;
   }
+  
+  public MouseListener getMouseListener() {return mouse_listener;}
+  public MouseMotionListener getMouseMotionListener() {return mouse_motion_listener;}
+  public ImageIcon getBGImageIcon() {return bg_imageicon;}
 }
